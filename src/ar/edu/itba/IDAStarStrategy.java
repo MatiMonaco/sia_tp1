17fd1b3b9e9fca69a4e9655e003fe8201630e824
igvazquez <ignavazquez9@gmail.com>
2020-08-19 00:24:50 -0300

    merge con deadlock branch

--- src/ar/edu/itba/IDAStarStrategy.java
+++ src/ar/edu/itba/IDAStarStrategy.java
@@ -60,39 +60,6 @@ public class IDAStarStrategy extends InformedSearchStrategy {
         }
     }
 
-//    public int search(StateNode node, int g, int threshold, Board board){
-//
-//        int f = g + heuristic.apply(node, board);
-//
-//        if (f > threshold){
-//            return f;
-//        }
-//        if (board.isCompleted(node.getBags())){
-//            endNode = node;
-//            return -1;
-//        }
-//
-//        int min = Integer.MAX_VALUE;
-//        List<StateNode> successors = node.getChildren(board);
-//        for(StateNode successor : successors) {
-//
-//            int temp = search(successor, successor.pathCost, threshold, board);
-//
-//            if (board.isCompleted(successor.getBags())){
-//                endNode = node;
-//                return -1;
-//            }
-//
-//            if (temp < min){
-//                min = temp;
-//                System.out.printf("actualice min: %d\n", min);
-//            }
-//        }
-//
-//        return min;
-//    }
-
-
     public int search(int g, int bound, Board board){
 
         StateNode current = path.peek();
@@ -131,42 +98,4 @@ public class IDAStarStrategy extends InformedSearchStrategy {
 
         return min;
     }
-
-
-//    public StateNode search(int g, int bound, Board board){
-//
-//        StateNode current = path.peek();
-//        int f = g + heuristic.apply(current, board);
-//
-//        if (board.isCompleted(current.baggs) || f > bound){
-//            return current;
-//        }
-//
-//
-//        min = -1;
-//        StateNode found = null;
-//        List<StateNode> successors = current.getChildren(board);
-//        for(StateNode successor : successors) {
-//
-//            if (!path.contains(successor)){
-//
-//                path.push(successor);
-//
-//                current = search(current.pathCost, bound, board);
-//
-//                if (board.isCompleted(current.baggs)){
-//                    return current;
-//                }
-//
-//                if (current.pathCost < min){
-//                    min = current.pathCost;
-//                    System.out.printf("actualice min: %d", min);
-//                    found = current;
-//                }
-//                path.pop();
-//            }
-//        }
-//
-//        return found;
-//    }
 }

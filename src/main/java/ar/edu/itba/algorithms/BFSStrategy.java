d600efb6350b17f437f3d69c573055f57ab52478
igvazquez <ignavazquez9@gmail.com>
2020-08-19 22:08:20 -0300

    merge conflict

---
+++ src/main/java/ar/edu/itba/algorithms/BFSStrategy.java
@@ -0,0 +1,62 @@
+package ar.edu.itba.algorithms;
+
+import ar.edu.itba.Board;
+
+import ar.edu.itba.entities.Box;
+import ar.edu.itba.entities.Player;
+
+import java.util.*;
+
+public class BFSStrategy extends SearchStrategy {
+
+    private long expandedNodes = 0;
+
+    public BFSStrategy(boolean deadlockCheck) {
+        super("BFS",deadlockCheck);
+    }
+
+
+    @Override
+    public SearchResult findSolution(Board board)  {
+
+        //init
+        Set<StateNode> visited;
+        Queue<StateNode> frontier;
+        board.restartLevel();
+        visited = new HashSet<>();
+        frontier = new LinkedList<>();
+
+        Set<Box> set = new HashSet<>(board.getBoxes());
+
+        StateNode root = new StateNode(' ',new Player(board.getPlayer().getX(),board.getPlayer().getY()),set,null,0);
+        frontier.add(root);
+
+        while(!frontier.isEmpty()){
+
+            StateNode vertex = frontier.poll();
+            expandedNodes++;
+            visited.add(vertex);
+            List<StateNode> successors = vertex.getChildren(board);
+
+            for(StateNode successor : successors){
+
+                if(!visited.contains(successor) && !frontier.contains(successor)){
+                    if(board.isCompleted(successor.boxes)){
+                        String solution =getSolutionPath(successor);
+                        System.out.println("BFS Solution: " + solution);
+                        System.out.println("Solution length: "+solution.length());
+                        System.out.println("Expanded nodes: " + expandedNodes);
+                        return new SearchResult(name,null,successor, expandedNodes,frontier.size(), getSolutionPath(successor));
+                    }
+
+                    frontier.add(successor);
+
+                }
+            }
+        }
+        System.out.println("NO SOLUTION FOUND");
+        return new SearchResult(name,null,null, expandedNodes,frontier.size(), null);
+
+
+    }
+}
---
+++ src/main/java/ar/edu/itba/entities/Wall.java
@@ -0,0 +1,32 @@
+package ar.edu.itba.entities;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Wall extends Actor {
+
+    private Image image;
+
+    public Wall(int x, int y) {
+        super(x, y);
+        
+        initWall();
+    }
+    
+    private void initWall() {
+        
+        ImageIcon iicon = new ImageIcon(getClass().getClassLoader().getResource("wall.png"));
+        image = iicon.getImage();
+        setImage(image);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
